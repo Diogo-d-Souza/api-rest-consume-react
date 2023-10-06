1a9f1ad7590b3d5c9c1dad5ee0005032d83f3c59
Diogo <di_smgomes@hotmail.com>
2023-10-06 14:18:23 -0300

    feat: History

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { Router } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from './components/Header';
 import Routes from './routes';
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <Routes />
       <GlobalStyles />
-    </BrowserRouter>
+    </Router>
   );
 }
 
