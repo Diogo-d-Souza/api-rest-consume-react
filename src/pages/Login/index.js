97d160d6f76cc911de37211eab016f71d6b52669
Diogo <di_smgomes@hotmail.com>
2023-10-05 22:07:52 -0300

    feat: Adding style components and some colors

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from '../../styles/GlobalStyles';
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+      <p>pag log</p>
+      <a href="none">something</a>
+    </Container>
+  );
+}
